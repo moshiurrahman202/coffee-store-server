23979a2ff414ead871f17cc5dd030ea4fcdd2bcb
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-12-10 14:35:33 +0600

    recevied coffee data by post method and send data to mongodbbatabase

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-// const uri = "mongodb+srv://coffee-store:wQ8h9OtmGyqF8pVI@cluster0.ppjooy5.mongodb.net/?appName=Cluster0";
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ppjooy5.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,12 +24,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const coffeeCollection = client.db("coffeeDB").collection("coffees")
+
+    app.post("/coffees", async(req, res) => {
+      const newCoffee =req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result)
+      
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
