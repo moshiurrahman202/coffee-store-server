00893ca279fdeeb3cf1d823ad67458699b9ec9fe
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-12-11 16:04:14 +0600

    fefactor:  correctapi url

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function run() {
       res.send(result)
     })
 
-    app.get("/coffees/id", async(req, res) => {
+    app.get("/coffees/:id", async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await coffeeCollection.findOne(query);
