3cefd2a3fc3f2ffb73bb38de81a297d4775b870d
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-12-13 18:51:22 +0600

    feat: write delete api for delete user by id

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
       const result = await usersCollection.insertOne(userProfile);
       res.send(result)
     })
+
+    app.delete("/users/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
